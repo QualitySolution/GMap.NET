d4fc1cec5341923c523ed1c79c104fc1aa669b1d
Andrey Gankov <gav@qsolution.ru>
2016-07-14 19:51:25 +0300

    Added simple Gtk demo for test widget.

---
+++ Demo.GtkSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace Demo.GtkSharp
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.Init();
+			MainWindow win = new MainWindow();
+			win.Show();
+			Application.Run();
+		}
+	}
+}
